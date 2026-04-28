c29c5da68955005c239c59b026bb84852afce56c
mfaatihyusron <faatihyusron@gmail.com>
2026-04-28 20:26:53 +0700

    buat navbar + konsep fragment per page nya

--- app/src/main/java/com/example/timetracker/MainActivity.kt
+++ app/src/main/java/com/example/timetracker/MainActivity.kt
@@ -5,16 +5,53 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0) // Bottom padding handled by nav bar
             insets
         }
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_history -> {
+                    loadFragment(HistoryFragment())
+                    true
+                }
+                R.id.nav_play -> {
+                    loadFragment(PlayFragment())
+                    true
+                }
+                R.id.nav_reports -> {
+                    loadFragment(ReportsFragment())
+                    true
+                }
+                R.id.nav_settings -> {
+                    loadFragment(SettingsFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
