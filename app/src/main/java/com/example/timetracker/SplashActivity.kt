390f295b167a6e9d547b3f73d2ba1138613834c9
mfaatihyusron <faatihyusron@gmail.com>
2026-04-28 18:37:31 +0700

    Buat splashscren

--- app/src/main/java/com/example/timetracker/SplashActivity.kt
+++ app/src/main/java/com/example/timetracker/SplashActivity.kt
@@ -1,4 +1,22 @@
 package com.example.timetracker
 
-class SplashActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        
+        // Menunggu selama 3 detik sebelum pindah ke MainActivity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
 }
